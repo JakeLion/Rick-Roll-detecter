21ac40dbc241b319b2ee5582cd5a64bc26fd133f
JakeLion <69877521+JakeLion@users.noreply.github.com>
2021-04-09 16:33:57 -0600

    Add files via upload

---
+++ rickdetector.py
@@ -0,0 +1,15 @@
+while True:
+    link = input("Enter a link here")
+
+    if link == "https://www.youtube.com/watch?v=ub82Xb1C8os":
+        print("This is a Rick Roll\n")
+    elif link == "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO":
+        print("This is a Rick Roll\n")
+    elif link == "https://www.youtube.com/watch?v=6_b7RDuLwcI&ab_channel=Jaysean":
+        print("This is a Rock Roll\n")
+    elif link == "https://www.youtube.com/watch?v=Lrj2Hq7xqQ8&ab_channel=LuisRedsVidal":
+        print('This is a Rick Roll\n')
+    elif link == "https://www.youtube.com/watch?v=IO9XlQrEt2Y&ab_channel=RickAstleyVEVO":
+        print("This is a Rick Roll\n")
+    else:
+        print("This is not a Rick Roll\n")
